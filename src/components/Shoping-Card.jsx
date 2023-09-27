583a2b29020bca174b86e153b07e92b38f3676f0
melissasidisid <moimelly451@gmail.com>
2023-09-27 14:52:37 +0100

    feat: added a remove button and counting price

--- src/components/Shoping-Card.jsx
+++ src/components/Shoping-Card.jsx
@@ -2,9 +2,20 @@ import React, { useState } from "react"
 
 const ShoppingCart = () => {
   const [items, setItems] = useState([])
+  const [totalPrice, setTotalPrice] = useState(0)
 
   const addToCart = (product) => {
     setItems([...items, product])
+    setTotalPrice(totalPrice + product.price)
+  }
+
+  const removeFromCart = (product) => {
+    const index = items.indexOf(product)
+    if (index > -1) {
+      items.splice(index, 1)
+      setItems([...items])
+      setTotalPrice(totalPrice - product.price)
+    }
   }
 
   return (
@@ -12,9 +23,13 @@ const ShoppingCart = () => {
       <h1>Shopping Cart</h1>
       <ul>
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item.name} - ${item.price}
+            <button onClick={() => removeFromCart(item)}>Remove</button>
+          </li>
         ))}
       </ul>
+      <p>Total Price: ${totalPrice}</p>
       <button onClick={addToCart}>Add to Cart</button>
     </div>
   )
