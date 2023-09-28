015eb63ec452000ad3b9e6dc7d17a6193ec4164b
BARKA Oussama <62702326+0m3ga13@users.noreply.github.com>
2023-09-28 22:53:26 +0100

    feat(navbar): add Links in the navbar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -32,69 +32,20 @@ export default function Card({ product }) {
             <p className="text-xs overflow-hidden whitespace-nowrap text-ellipsis text-content">
               {description}
             </p>
-            {/* <div className="flex items-center mt-2.5 mb-5">
-            <svg
-              className="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-content"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <span className="bg-accent text-content text-xs font-semibold mr-2 px-2.5 py-0.5 rounded ml-3">
-              5.0
-            </span>
-          </div> */}
-          <div className="flex items-center justify-between mt-3 mb-2">
-            <span className="text-xl font-bold text-content">{price}$</span>
-          </div>
-          <div className="flex flex-col items-left justify-between space-y-2">
-            <Button
-              productObject={product}
-              styling={
-                "w-full text-white uppercase  bg-accent hover:bg-bkgHover hover:text-black focus:ring-4 focus:outline-none focus:ring-content/25  font-medium rounded-lg text-xs p-2 text-center"
-              }
-            />
 
-            <Wishlist productObject={product} />
+            <div className="flex items-center justify-between mt-3 mb-2">
+              <span className="text-xl font-bold text-content">{price}$</span>
+            </div>
+            <div className="flex flex-col items-left justify-between space-y-2">
+              <Button
+                productObject={product}
+                styling={
+                  "w-full text-white uppercase  bg-accent hover:bg-bkgHover hover:text-black focus:ring-4 focus:outline-none focus:ring-content/25  font-medium rounded-lg text-xs p-2 text-center"
+                }
+              />
 
+              <Wishlist productObject={product} />
+            </div>
           </div>
         </motion.div>
       </LazyMotion>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import Image from "next/image"
 // import logo from "logo."
-import { FaShoppingCart } from "react-icons/fa"
+import { FaShoppingCart, FaHeart } from "react-icons/fa"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div className="w-full border-2 border-border text-white">
+    <div className="w-full border-2 border-border text-white shadow-md">
       <div className="max-w-container mx-auto h-20 flex items-center">
         {/* ------  navbar-logo ------  */}
 
-        <div className="flex items-center h-20 border-e border-border p-4 hover:bg-slate-50 cursor-pointer">
+        <div className="flex items-center h-10  border-border p-4 hover:bg-slate-50 cursor-pointer">
           <a href="#">
             {" "}
             <Image
@@ -26,39 +27,46 @@ const Navbar = () => {
 
         <div>
           <nav class="flex justify-center items-center space-x-2  text-zinc-500">
-            <a
-              href="/men"
+            <Link
+              href="/"
               class="items-center flex font-medium px-8 py-2 h-20 rounded-lg hover:bg-slate-100 hover:text-primary"
             >
-              MEN
-            </a>
-            <a
-              href="/women"
+              Dashboard
+            </Link>
+            <Link
+              href="/products"
               class="items-center flex font-medium px-8 py-2 h-20 rounded-lg hover:bg-slate-100 hover:text-primary"
             >
-              WOMEN
-            </a>
-            <a
-              href="/kids"
+              Shop
+            </Link>
+            <Link
+              href="/about"
               class="items-center flex font-medium px-8 py-2 h-20 rounded-lg hover:bg-slate-100 hover:text-primary"
             >
-              KIDS
-            </a>
-            <a
-              href="/new arivals"
+              About us
+            </Link>
+            <Link
+              href="/contact"
               class="items-center flex font-medium px-8 py-2 h-20 rounded-lg hover:bg-slate-100 hover:text-primary"
             >
-              New Arrivals
-            </a>
+              Contact us
+            </Link>
 
             {/* ------ cart icon ------  */}
 
-            <a
-              href="#"
-              className="absolute right-8 flex rounded-lg hover:bg-slate-100 hover:text-primary"
+            <Link
+              href="/wishlist"
+              className="absolute right-20 flex rounded-lg hover:bg-slate-100 hover:text-primary"
             >
-              <FaShoppingCart className="  h-8 w-8 " />{" "}
-            </a>
+              <FaHeart className="  h-8 w-8 hover:text-red-600 hover:h-6 transition-all ease-in-out duration-300" />{" "}
+            </Link>
+
+            <Link
+              href="/cart"
+              className="absolute right-8 flex rounded-lg hover:bg-slate-100 hover:text-primary "
+            >
+              <FaShoppingCart className="  h-8 w-8 hover:text-accent hover:h-6 hover:rotate-6 transition-all ease-in-out duration-300" />{" "}
+            </Link>
           </nav>
         </div>
       </div>
