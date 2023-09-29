2906d70037b2315d4e25a002fe1e9e57c309b79e
melissasidisid <moimelly451@gmail.com>
2023-09-29 22:50:43 +0100

    feat: added filter option

--- src/components/Shoping-Card.jsx
+++ src/components/Shoping-Card.jsx
@@ -1,38 +1,56 @@
 import React, { useState } from "react"
+import { Link } from "react-router-dom"
 
-const ShoppingCart = () => {
+const Wishlist = () => {
   const [items, setItems] = useState([])
   const [totalPrice, setTotalPrice] = useState(0)
+  const [count, setCount] = useState(1)
 
-  const addToCart = (product) => {
-    setItems([...items, product])
-    setTotalPrice(totalPrice + product.price)
+  const addToCart = (product, count) => {
+    setItems([...items, { product, count }])
+    setTotalPrice(totalPrice + product.price * count)
   }
 
   const removeFromCart = (product) => {
-    const index = items.indexOf(product)
-    if (index > -1) {
-      items.splice(index, 1)
-      setItems([...items])
-      setTotalPrice(totalPrice - product.price)
-    }
+    const updatedItems = items.filter((item) => item.product.id !== product.id)
+    setItems(updatedItems)
+    setTotalPrice(totalPrice - product.price * product.count)
   }
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {item.name} - ${item.price}
-            <button onClick={() => removeFromCart(item)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-      <p>Total Price: ${totalPrice}</p>
-      <button onClick={addToCart}>Add to Cart</button>
-    </div>
+    <>
+      <div className="card">
+        <div className="container">
+          <div>
+            <img src={img} alt="no image" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Shopping Cart</h1>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              <div>{item.count} qte</div>
+              <button onClick={() => setCount((count) => count + 1)}>+</button>
+              <button
+                onClick={() => setCount((count) => (count > 0 ? count - 1 : 0))}
+              >
+                -
+              </button>
+              <button onClick={() => setCount(1)}>reset</button>
+              {item.product.name} - ${item.product.price}
+              <button onClick={() => removeFromCart(item.product)}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p>Total Price: ${totalPrice}</p>
+        <button onClick={() => addToCart(product, count)}>Add to Cart</button>
+      </div>
+    </>
   )
 }
 
-export default ShoppingCart
+export default Wishlist
